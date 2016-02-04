0416ff90f91dafb8f5df773250e9d80d33cefbc6
Koelli91 <kevinkoellmann@outlook.com>
2016-02-04 04:38:32 +0100

    Moved Test-Site to own WebStorage; added Banner-Logo

---
+++ js/jquery.imgCrossfade.js
@@ -0,0 +1,57 @@
+(function ( $ ) {
+	
+	$.fn.imgCrossfade = function( options ) {
+		
+		function injectStyles() {
+			var styles =
+"<style>\
+	.cf-container { position: relative; }\n\
+	.cf-container img {\n\
+		position: absolute;\n\
+		top: 0;\n\
+		left: 0;\n\
+		z-index: 1;\n\
+		max-width: 100%;\n\
+		max-height: 100%;\n\
+		display: block;\n\
+	}\n\
+	.cf-container img.cf-base {\n\
+		position: static;\n\
+		z-index: -1;\n\
+		visibility: hidden;\n\
+	}\n\
+	.cf-container img.cf-active {\n\
+		z-index: 3;\n\
+	}\n\
+</style>";
+		$('head').append(styles);
+		}
+		
+		var settings = $.extend({
+			fading: 'medium',
+			interval: 7000
+		}, options);
+		
+		settings.fading = ( $.isNumeric(settings.fading) ? settings.fading : settings.fading == 'fast' ? 750 : settings.fading == 'slow' ? 2500 : 1500 );
+		if ( settings.interval < settings.fading ) settings.interval = settings.fading;
+		
+		injectStyles();
+		
+		return this.each(function() {
+			var cycler = $(this);
+			cycler.addClass('cf-container');
+			cycler.find('img:first').clone().prependTo(this).addClass('cf-base');
+			cycler.find('img:nth-child(2)').addClass('cf-active');
+
+			window.setInterval(function() {
+				var active = cycler.find('.cf-active');
+				var next = (active.next(':not(.cf-base)').length > 0 ? active.next(':not(.cf-base)') : cycler.find('img:not(.cf-base)').first());
+				next.css('z-index', 2); // move the next image up the pile
+				active.fadeOut(settings.fading, function() { // fade out the top image
+					active.css('z-index', 1).show().removeClass('cf-active'); //reset the z-index and unhide the image
+					next.css('z-index', 3).addClass('cf-active'); // make the next image the top one
+				});
+			}, settings.interval);
+		});
+	}
+}( jQuery ));
